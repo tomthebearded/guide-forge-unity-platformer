32459dbf7659e6ba99ced3b1f3e394551ddadb75
Tommaso Mastroberardino <tomthebearded@hotmail.com>
2026-09-07 10:47:21 +0200

    feat(ui): add a coins and lives HUD driven by gameplay events

---
+++ cavern-dash/Assets/_Project/Scripts/HudView.cs
@@ -0,0 +1,32 @@
+using TMPro;
+using UnityEngine;
+
+public class HudView : MonoBehaviour
+{
+    [SerializeField] private TMP_Text coinsLabel;
+    [SerializeField] private TMP_Text livesLabel;
+
+    [SerializeField] private PlayerStats stats;
+    [SerializeField] private PlayerHealth health;
+
+    private void OnEnable()
+    {
+        stats.CoinsChanged += ShowCoins;
+        health.LivesChanged += ShowLives;
+
+        ShowCoins(stats.CoinsCollected);
+        ShowLives(health.LivesRemaining);
+    }
+
+    private void OnDisable()
+    {
+        stats.CoinsChanged -= ShowCoins;
+        health.LivesChanged -= ShowLives;
+    }
+
+    private void ShowCoins(int coinsCollected) =>
+        coinsLabel.text = $"Coins: {coinsCollected}";
+
+    private void ShowLives(int livesRemaining) =>
+        livesLabel.text = $"Lives: {livesRemaining}";
+}
